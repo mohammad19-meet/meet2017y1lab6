aac9f580047909d1901f3af390a014f4a97d8c2f
mohammad19-meet <mohammad19@meet.mit.edu>
2017-07-31 17:24:00 +0300

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,14 @@
+import turtle
+"""
+turtle.shape('turtle')
+square = turtle.clone()"""
+turtle.shape('square')
+turtle.goto(100,0)
+turtle.goto(100,-100)
+turtle.goto(0,-100)
+turtle.goto(0,0)
+
+
+turtle.shape('triangle')
+turtle.goto(50,100)
+turtle.goto(100,0)
